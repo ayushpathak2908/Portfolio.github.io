7404e498de4f09cc3bd5eb2db54c0948f7f87d6d
ayushpathak2908 <84396325+ayushpathak2908@users.noreply.github.com>
2021-07-15 13:41:02 +0530

    my portfolio

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+var hamberger = document.querySelector('.hamberger');
+var times = document.querySelector('.times');
+var mobileNav = document.querySelector('.mobile-nav');
+
+hamberger.addEventListener('click', function(){
+  mobileNav.classList.add('open');
+});
+
+times.addEventListener('click', function(){
+
+});
+
+
+});
+
